05c4ba04972b27f32a5b8cff36eb11435925804c
benjamin <be.malvino@duocuc.cl>
2025-05-13 15:40:37 -0400

    login

--- TecnoStore/ventas/urls.py
+++ TecnoStore/ventas/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views import home, galeria, base
+from django.contrib.auth import views as auth_views
+from . import views
 # Define the URL patterns for the ventas app    
 
 urlpatterns = [
     path('', home, name='home'),
     path('galeria/', galeria, name='galeria'),
     path('base/', base, name='base'),
+    path('login/', auth_views.LoginView.as_view(template_name='ventas/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('register/', views.register, name='register'),
     
 ]
--- TecnoStore/ventas/views.py
+++ TecnoStore/ventas/views.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 #funcion para mostrar vistas
 def home(request):
@@ -10,3 +13,13 @@ def base(request):
 
 def galeria(request):
     return render(request, 'ventas/galeria.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'ventas/register.html', {'form': form})
