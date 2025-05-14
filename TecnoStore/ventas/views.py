7c0ddd7244bf9453e6066858470fffd7778cf3ec
camiiloo <camiiloozamora@gmail.com>
2025-05-14 12:18:29 -0400

    Arreglos m

--- TecnoStore/ventas/views.py
+++ TecnoStore/ventas/views.py
@@ -1,6 +1,7 @@
 # Import necessary modules
 from django.shortcuts import redirect, render
-from .models import Producto
+from .models import Producto, Carrito
+from .carrito import Carrito
 from .forms import ProductoForm, CustomUserCreationForm
 from django.contrib import messages
 from django.contrib.auth import login, authenticate
@@ -21,7 +22,8 @@ def base(request):
 def galeria(request):
     return render(request, 'ventas/galeria.html')
 
-def agregar_producto(request):
+#Producotps 
+def agregar_productos(request):
     data = {
         'form': ProductoForm()
     }
@@ -35,13 +37,6 @@ def agregar_producto(request):
 
     return render(request, 'ventas/producto/agregar.html',data)
 
-def listar_productos(request):
-    productos = Producto.objects.all()
-    data = {
-        'productos': productos
-    }
-
-    return render(request, 'ventas/producto/listar.html', data)
 def registro(request):
     data = {
         'form': CustomUserCreationForm()
@@ -58,3 +53,43 @@ def registro(request):
             data["form"] = formulario
 
     return render(request, 'registration/registro.html', data)
+
+def carrito(request):
+    return render(request, 'tienda/carrito.html')
+
+###carritoo
+def tienda(request):
+    productos = Producto.objects.all()
+    return render(request, "carrito.html", {'productos':productos})
+
+def agregar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.agregar(producto)
+    return redirect("carrito")
+
+def eliminar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.eliminar(producto)
+    return redirect("carrito")
+
+def restar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.restar(producto)
+    return redirect("carrito")
+
+def limpiar_carrito(request):
+    carrito = Carrito(request)
+    carrito.limpiar()
+    return redirect("home") ## -->
+
+# listar
+def listar_productos(request):
+    productos = Producto.objects.all()
+    data = {
+        'productos': productos
+    }
+
+    return render(request, 'ventas/producto/listar.html', data)
